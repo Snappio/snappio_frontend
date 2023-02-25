61a56aba1732e5b06b86f8e91692285c8483d4ad
Pradipto-m <pro.edu08@gmail.com>
2023-02-25 11:04:32 +0530

    create assets

---
+++ lib/themes.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class ThemeModes {
+  static ThemeData lightMode(BuildContext context) => ThemeData(
+
+  );
+
+  static ThemeData darkMode(BuildContext context) => ThemeData(
+
+  );
+}
