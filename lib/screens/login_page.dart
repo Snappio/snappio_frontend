80b4bfe1228348853cddced1b6616c321ff932ea
Pradipto-m <pro.edu08@gmail.com>
2023-03-03 16:11:56 +0530

    Feat: Add icons in text form

--- lib/screens/login_page.dart
+++ lib/screens/login_page.dart
@@ -19,82 +19,87 @@ class _LoginPageState extends State<LoginPage> {
 
   @override
   Widget build(BuildContext context) {
+    // Size size = MediaQuery.of(context).size;
+    
     return Scaffold(
       resizeToAvoidBottomInset: false,
-      body: SafeArea(
+      body: SingleChildScrollView(
+        child: SafeArea(
           child: Column(
-        children: [
-          Image.asset("assets/images/login.png"),
-          Container(
-            padding: const EdgeInsets.symmetric(horizontal: 35),
-            width: double.infinity,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                const Text("Welcome to Snappio",
-                    style: TextStyle(fontWeight: FontWeight.bold),
-                    textScaleFactor: 2.4),
-                const SizedBox(height: 75),
-                TextFormField(
-                    decoration: const InputDecoration(
-                  labelText: "Username",
-                  // labelStyle: TextStyle(fontSize: 18),
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(16))),
-                  // filled: true,
-                  contentPadding: EdgeInsets.all(14),
-                )),
-                const SizedBox(height: 20),
-                TextFormField(
-                    decoration: const InputDecoration(
-                      labelText: "Password",
-                      // labelStyle: TextStyle(fontSize: 18),
-                      border: OutlineInputBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(16))),
-                      // filled: true,
-                      contentPadding: EdgeInsets.all(14),
-                    ),
-                    obscureText: true),
-                const SizedBox(height: 60),
-                InkWell(
-                  onTap: () => loginPressed(context),
-                  child: AnimatedContainer(
-                      duration: const Duration(milliseconds: 350),
-                      padding: const EdgeInsets.symmetric(vertical: 12),
-                      width: double.infinity,
-                      alignment: Alignment.center,
-                      decoration: ShapeDecoration(
-                          shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(16)),
-                          color: login
-                              ? Colors.transparent
-                              : Theme.of(context).cardColor),
-                      child: login
-                          ? const CircularProgressIndicator()
-                          : const Text("Sign In",
-                              style: TextStyle(fontWeight: FontWeight.bold),
-                              textScaleFactor: 1.4)),
-                ),
-                const SizedBox(height: 15),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset("assets/images/login.png"),
+              Container(
+                padding: const EdgeInsets.symmetric(horizontal: 35),
+                width: double.infinity,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
-                    const Text("Don't have an account? "),
-                    InkWell(
-                      onTap: () => Navigator.of(context).push(
-                        MaterialPageRoute(builder: (context) => const SignupPage())),
-                      child: const Text(" Sign Up",
-                          style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              color: Themes.darkAccent)),
-                    )
-                  ],
-                )
-              ],
-            ),
-          )
-        ],
-      )),
-    );
+                    const Text("Welcome to Snappio",
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                      textScaleFactor: 2.4),
+                    const SizedBox(height: 75),
+                    TextFormField(
+                      decoration: const InputDecoration(
+                        labelText: "Username",
+                        prefixIcon: Icon(Icons.account_circle_rounded),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(25))),
+                        // filled: true,
+                        contentPadding: EdgeInsets.all(15),
+                      )),
+                    const SizedBox(height: 20),
+                    TextFormField(
+                      decoration: const InputDecoration(
+                        labelText: "Password",
+                        prefixIcon: Icon(Icons.verified_user),
+                        border: OutlineInputBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(25))),
+                        // filled: true,
+                        contentPadding: EdgeInsets.all(15),
+                      ),
+                      obscureText: true),
+                  const SizedBox(height: 60),
+                  InkWell(
+                    onTap: () => loginPressed(context),
+                    child: AnimatedContainer(
+                        duration: const Duration(milliseconds: 350),
+                        padding: const EdgeInsets.symmetric(vertical: 12),
+                        width: double.infinity,
+                        alignment: Alignment.center,
+                        decoration: ShapeDecoration(
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(25)),
+                            color: login
+                                ? Colors.transparent
+                                : Theme.of(context).cardColor),
+                        child: login
+                            ? const CircularProgressIndicator()
+                            : const Text("Sign In",
+                                style: TextStyle(fontWeight: FontWeight.bold),
+                                textScaleFactor: 1.4)),
+                  ),
+                  const SizedBox(height: 15),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text("Don't have an account? "),
+                      InkWell(
+                        onTap: () => Navigator.of(context).push(
+                            MaterialPageRoute(
+                                builder: (context) => const SignupPage())),
+                        child: const Text(" Sign Up",
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                color: Themes.darkAccent)),
+                      ),
+                    ],
+                  ),
+                ],
+              )),
+          ],
+          ),
+        )
+      ));
   }
 }
--- lib/screens/signup_page.dart
+++ lib/screens/signup_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:snappio_frontend/screens/login_page.dart';
 import 'package:snappio_frontend/themes.dart';
 
 class SignupPage extends StatefulWidget {
@@ -27,114 +26,119 @@ class _SignupPageState extends State<SignupPage> {
 
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+
     return Scaffold(
       resizeToAvoidBottomInset: false,
-      body: SingleChildScrollView(
-        child: SafeArea(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Expanded(
             child: Column(
-          children: [
-            Image.asset("assets/images/signup.png"),
-            Container(
-              padding: const EdgeInsets.symmetric(horizontal: 35),
-              width: double.infinity,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  const Text("Join Snappio",
-                      style: TextStyle(fontWeight: FontWeight.bold),
-                      textScaleFactor: 2.6),
-                  const SizedBox(height: 60),
-                  Form(
-                    key: _formKey,
-                    child: Column(children: [
-                      TextFormField(
-                        decoration: const InputDecoration(
+            children: [
+              Image.asset("assets/images/signup.png"),
+              Container(
+                padding: const EdgeInsets.symmetric(horizontal: 35),
+                width: double.infinity,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    const Text("Join Snappio",
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                        textScaleFactor: 2.6),
+                    const SizedBox(height: 60),
+                    Form(
+                      key: _formKey,
+                      child: Column(children: [
+                        TextFormField(
+                          decoration: const InputDecoration(
+                            prefixIcon: Icon(Icons.account_circle_rounded),
                             labelText: "Username",
-                            // labelStyle: TextStyle(fontSize: 18),
                             border: OutlineInputBorder(
                                 borderRadius:
-                                    BorderRadius.all(Radius.circular(16))),
+                                  BorderRadius.all(Radius.circular(25))),
                             // filled: true,
-                            contentPadding: EdgeInsets.all(14)),
-                        validator: (value) {
-                          if (value!.isEmpty) return "Field cannot be empty";
-                          return null;
-                        },
-                      ),
-                      const SizedBox(height: 20),
-                      TextFormField(
-                        decoration: const InputDecoration(
-                          labelText: "Email",
-                          // labelStyle: TextStyle(fontSize: 18),
-                          border: OutlineInputBorder(
-                              borderRadius:
-                                  BorderRadius.all(Radius.circular(16))),
-                          // filled: true,
-                          contentPadding: EdgeInsets.all(14),
+                            contentPadding: EdgeInsets.all(15)),
+                            validator: (value) {
+                              if (value!.isEmpty) return "Field cannot be empty";
+                              return null;
+                            },
                         ),
-                        keyboardType: TextInputType.emailAddress,
-                        onChanged: (value) {
-                          email = value;
-                          setState(() {});
-                        },
-                        validator: (value) {
-                          return validEmail() ? null : "Invalid email";
-                        },
-                      ),
-                      const SizedBox(height: 20),
-                      TextFormField(
-                        decoration: const InputDecoration(
-                            labelText: "Password",
-                            // labelStyle: TextStyle(fontSize: 18),
+                        const SizedBox(height: 20),
+                        TextFormField(
+                          decoration: const InputDecoration(
+                            prefixIcon: Icon(Icons.email_rounded),
+                            labelText: "Email",
                             border: OutlineInputBorder(
                                 borderRadius:
-                                    BorderRadius.all(Radius.circular(16))),
+                                    BorderRadius.all(Radius.circular(25))),
                             // filled: true,
-                            contentPadding: EdgeInsets.all(14)),
-                        obscureText: true,
-                        validator: (value) {
-                          if (value!.length < 6) {
-                            return "Password must be atleast 6 characters";
-                          }
-                          return null;
-                        },
-                      ),
-                    ]),
-                  ),
-                  const SizedBox(height: 50),
-                  InkWell(
-                    onTap: () => signupPressed(context),
-                    child: Container(
-                        padding: const EdgeInsets.symmetric(vertical: 12),
-                        width: double.infinity,
-                        alignment: Alignment.center,
-                        decoration: ShapeDecoration(
-                            shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(16)),
-                            color: Theme.of(context).cardColor),
-                        child: const Text("Sign Up",
-                            style: TextStyle(fontWeight: FontWeight.bold),
-                            textScaleFactor: 1.4)),
-                  ),
-                  const SizedBox(height: 15),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      const Text("Already have an account? "),
-                      InkWell(
-                        onTap: () => Navigator.of(context).pop(),
-                        child: const Text(" Sign In",
-                            style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                color: Themes.darkAccent)),
-                      )
-                    ],
-                  )
-                ],
-              ),
-            )
-          ],
-        )),
+                            contentPadding: EdgeInsets.all(15),
+                          ),
+                          keyboardType: TextInputType.emailAddress,
+                          onChanged: (value) {
+                            email = value;
+                            setState(() {});
+                          },
+                          validator: (value) {
+                            return validEmail() ? null : "Invalid email";
+                          },
+                        ),
+                        const SizedBox(height: 20),
+                        TextFormField(
+                          decoration: const InputDecoration(
+                              prefixIcon: Icon(Icons.verified_user),
+                              labelText: "Password",
+                              border: OutlineInputBorder(
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(25))),
+                              // filled: true,
+                              contentPadding: EdgeInsets.all(15)),
+                          obscureText: true,
+                          validator: (value) {
+                            if (value!.length < 6) {
+                              return "Password must be atleast 6 characters";
+                            }
+                            return null;
+                          },
+                        ),
+                      ]),
+                    ),
+                    const SizedBox(height: 50),
+                    InkWell(
+                      onTap: () => signupPressed(context),
+                      child: Container(
+                          padding: const EdgeInsets.symmetric(vertical: 12),
+                          width: double.infinity,
+                          alignment: Alignment.center,
+                          decoration: ShapeDecoration(
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(25)),
+                              color: Theme.of(context).cardColor),
+                          child: const Text("Sign Up",
+                              style: TextStyle(fontWeight: FontWeight.bold),
+                              textScaleFactor: 1.4)),
+                    ),
+                    const SizedBox(height: 15),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Text("Already have an account? "),
+                        InkWell(
+                          onTap: () => Navigator.of(context).pop(),
+                          child: const Text(" Sign In",
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  color: Themes.darkAccent)),
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              )
+            ],
+            ),
+          ),
+        ),
       ),
     );
   }
